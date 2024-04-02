7c0d9e7d6a1bae6c4a1b6ea077395688c771b574
Corey Walo <cjwalo@gmail.com>
2024-04-02 16:55:27 -0700

    Rename package

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "Discogs",
+    name: "DiscogsSwift",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Discogs",
-            targets: ["Discogs"]),
+            name: "DiscogsSwift",
+            targets: ["DiscogsSwift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Discogs"),
+            name: "DiscogsSwift"),
         .testTarget(
-            name: "discogs-swiftTests",
-            dependencies: ["discogs-swift"]),
+            name: "DiscogsSwiftTests",
+            dependencies: ["DiscogsSwift"]),
     ]
 )
