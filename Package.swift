8f3e05f1877fa0fb3dc835472a5947f2126528e7
Corey Walo <cjwalo@gmail.com>
2024-04-02 16:25:55 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "discogs-swift",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "discogs-swift",
+            targets: ["discogs-swift"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "discogs-swift"),
+        .testTarget(
+            name: "discogs-swiftTests",
+            dependencies: ["discogs-swift"]),
+    ]
+)
---
+++ Tests/discogs-swiftTests/discogs_swiftTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import discogs_swift
+
+final class discogs_swiftTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
